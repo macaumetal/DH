6d36dc58b4386063bbed3a347f62b9e62152bfec
Gustavo <macaumetal@hotmail.com>
2018-06-08 17:39:32 -0300

    Clase 03 de PDO - Subir Archivo

--- clase-02-PDO/agregarPelicula.php
+++ clase-02-PDO/agregarPelicula.php
@@ -1,27 +1,56 @@
 <?php
 if ($_POST){
-
+// OPIONES DE CONEXION
 $dsn = 'mysql:host=localhost;dbname=movies_db;charset=utf8mb4';
 $db_user = 'root';
 $db_pass = '';
+
+//CREACION DEL PDO
 $db = new PDO($dsn, $db_user, $db_pass);//creo el nuevo objeto
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-$stmt = $db->prepare ('INSERT INTO movies (title, rating, awards, length, release_date) VALUES (:title, :rating, :awards, :length, :release_date)');
+
+
+//*VER SI EXITE EL TITULO A AGREGAR
+
+try{
+    $existe= "SELECT id FROM movies WHERE title=:title":
+
+
+    $queryPreparada = $db->prepare($existe);
+    $queryPreparada->bindParam(":title", $title, PDO::PARAM_STR);
+
+    $title = $_POST['titulo'];
+
+    $queryPreparada->execute();
+
+    $resultado = $queryPreparada->fetch(PDO::FETCH_ASSOC);
+}
 
 
 
-$title = $_POST['titulo'];
-$rating = $_POST['rating'];
-$awards = $_POST['premios'];
-$length = $_POST['duracion'];
-$release_date = $_POST['anio'].'-'.$_POST['mes'].'-'.$_POST['dia'];
+
+//STATEMENT Y PREPARACION DE LA QUERY
+$stmt = $db->prepare ('INSERT INTO movies (title, rating, awards, length, release_date, genre_id) VALUES (:title, :rating, :awards, :length, :release_date, :genre_id)');
+
+//BINDEO
 $stmt->bindParam(":title", $title, PDO::PARAM_STR);
 $stmt->bindParam(":rating", $rating, PDO::PARAM_INT);
 $stmt->bindParam(":awards", $awards, PDO::PARAM_STR);
 $stmt->bindParam(":length", $length, PDO::PARAM_STR);
 $stmt->bindParam(":release_date", $release_date, PDO::PARAM_STR);
+$stmt->bindParam(":genre_id", $genre_id, PDO::PARAM_STR);
+
+//ASIGNACION DE VALORES
+$title = $_POST['titulo'];
+$rating = $_POST['rating'];
+$awards = $_POST['premios'];
+$length = $_POST['duracion'];
+$release_date = $_POST['anio'].'-'.$_POST['mes'].'-'.$_POST['dia'];
+$genre_id = $_POST['genero']; 
+
+
 
 $stmt->execute(); 
 
@@ -30,14 +59,9 @@ $stmt->execute();
 
 
 
-
-
 }
-$dsn = 'mysql:host=localhost;dbname=movies_db;charset=utf8mb4';
-$db_user = 'root';
-$db_pass = '';
-$db = new PDO($dsn, $db_user, $db_pass);
-$statement = $db->query("SELECT id, name FROM genres");
+
+$statement = $db->query("SELECT id, name FROM genres ORDER BY name");
 $resultado = $statement->fetchAll(PDO::FETCH_ASSOC); // es un metodo para que nos traiga todo en modo de array
 
 
@@ -81,23 +105,18 @@ var_dump($resultado);
                     <?php } ?>
                 </select>
                 <select name="anio">
-                    <option value="">Anio</option>
+                    <option value="">Año</option>
                     <?php for ($i=1900; $i < 2017; $i++) { ?>
                         <option value="<?php echo $i; ?>"><?php echo $i; ?></option>
                     <?php } ?>
                 </select><br>
-                <label>Genero</label>
-                <select name="genero">
-                    <option value="<?php echo $value[$clave]; ?>"><?php foreach ($resultado as $key => $value) {
-                                        foreach ($value as $clave => $valor) {
+                <label for="genero">Genero</label>
+                <select name="genero" id="genero">
+                    <option value="" selected disabled=>Elija genero</option>
+                    <?php foreach ($resultado as $value): ?>
+                        <option value="<?php echo $value['id']?>"> <?php echo $value['name'] ?></option>
+                    <?php endforeach ?>
 
-                                            echo $value[$clave];
-                                            echo"<br>";
-                                        }
-        
-                                        } ?>
-                                                
-                    </option>
                             
                     
                 </select>
---
+++ clase-03-PDO/subir.php
@@ -0,0 +1,42 @@
+<?php
+if ($_POST){
+	function subirArchivo () {
+var_dump($_FILES ['fotoPerfil']['error']);
+		if ($_FILES ['fotoPerfil']['error'] === UPLOAD_ERR_OK){
+			$archivoSubido = $_FILES ['fotoPerfil']['name'];
+			$archivoTemp = $_FILES['fotoPerfil']['tmp_name'];
+			$extensionDeArchivo = pathinfo($archivoSubido, PATHINFO_EXTENSION);
+
+			$laFotodePerfil = dirname(__FILE__);
+			$laFotodePerfil = $laFotodePerfil . '/subidos/';
+			$laFotodePerfil = $laFotodePerfil . uniqid() . "." . $extensionDeArchivo;
+
+			/*var_dump($laFotodePerfil);*/
+			move_uploaded_file($archivoTemp, $laFotodePerfil);
+
+			
+		}
+	}
+	subirArchivo();
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<form method="post" enctype="multipart/form-data">
+	<input type="hidden" name="MAX_FILE_SIZE" value=100000000>
+	<input type="file" name="fotoPerfil"><br>
+	<input type="submit" name="Subir">
+	
+
+
+</form>
+
+
+
+</body>
+</html>
